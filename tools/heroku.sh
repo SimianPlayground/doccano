685b0ef0664ca6f31f7aff95ad59e3286f77f9ec
Di Wu <diwu1989@users.noreply.github.com>
2020-07-21 12:05:36 -0700

    Update heroku.sh

--- tools/heroku.sh
+++ tools/heroku.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -o errexit
+python app/manage.py wait_for_db
+python app/manage.py migrate
+python app/manage.py create_roles
 
 if [ -n "$ADMIN_USER_NAME" ]; then
     python app/manage.py create_admin --noinput --username="$ADMIN_USER_NAME" --email="$ADMIN_CONTACT_EMAIL" --password="$ADMIN_PASSWORD"
